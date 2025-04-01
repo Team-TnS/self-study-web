7489424f3c530149f68d877ac28a9058abcf8eab
tulee <tulee@12cm.co.kr>
2025-04-02 03:06:54 +0900

    feat : 메인 -> 책장 -> 책 -> 문제 페이지 생성

---
+++ src/app/(pages)/book/[Id]/page.tsx
@@ -0,0 +1,55 @@
+"use client"
+
+import styles from "./page.module.css"
+import { useEffect, use } from "react"
+import BasicButton from "@/components/input/BasicButton"
+import BasicText from "@/components/display/BasicText"
+import { usePageHeader } from "@/layout/PageHeaderProvider"
+import { useRouter } from "next/navigation"
+
+export default function BookPage({
+  params,
+}: {
+  params: Promise<{ Id: string }>
+}) {
+  const router = useRouter()
+  const { Id } = use(params)
+  const { setTitle } = usePageHeader()
+
+  useEffect(() => {
+    setTitle(Id)
+  }, [])
+
+  const questions = [
+    { name: "1번 문제", isComplete: true },
+    { name: "2번 문제", isComplete: true },
+    { name: "3번 문제", isComplete: false },
+  ]
+  return (
+    <div className={styles.page}>
+      {!questions ? (
+        <>
+          <BasicText text={"등록된 문제가 없습니다."} />
+          <BasicButton
+            text={"문제 등록하기"}
+            onPress={() => router.push("/question/register")}
+          />
+        </>
+      ) : (
+        <div>
+          {Id}
+          {questions.map((question) => (
+            <div key={question.name} className={styles.questionItem}>
+              <BasicText text={question.name} />
+              <BasicText text={question.isComplete.toString()} />
+            </div>
+          ))}
+          <BasicButton
+            text={"문제 등록하기"}
+            onPress={() => router.push("/question/register")}
+          />
+        </div>
+      )}
+    </div>
+  )
+}
--- src/app/(pages)/book/register/page.tsx
+++ src/app/(pages)/book/register/page.tsx
@@ -4,19 +4,19 @@ import styles from "./page.module.css"
 import { useEffect } from "react"
 import { usePageHeader } from "@/layout/PageHeaderProvider"
 import TextForm from "@/components/input/TextForm"
-export default function ShelfRegisterPage() {
+export default function BookRegisterPage() {
   const { setTitle } = usePageHeader()
 
   useEffect(() => {
-    setTitle("책장 만들기")
+    setTitle("책 만들기")
   }, [])
 
   return (
     <div className={styles.page}>
       <TextForm
         maxLength={24}
-        placeholder={"책장 이름을 입력해주세요."}
-        buttonText={"책장 만들기"}
+        placeholder={"책 이름을 입력해주세요."}
+        buttonText={"책 만들기"}
       />
     </div>
   )
